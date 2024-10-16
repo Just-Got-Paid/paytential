2f91a329d33e4d6de79493065c3bbae8f223dceb
Z-noob <Zhenniwu5@gmail.com>
2024-10-16 15:26:55 -0400

    broken

--- server/middleware/checkAuthentication.js
+++ server/middleware/checkAuthentication.js
@@ -1,6 +1,7 @@
 // Is the user logged in?
 // Not specific user, just ANY user
 const checkAuthentication = (req, res, next) => {
+  console.log(req.session); // Log the session
   const { userId } = req.session;
   if (!userId) return res.sendStatus(401);
   return next();
