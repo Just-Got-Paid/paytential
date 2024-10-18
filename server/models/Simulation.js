0ee3edf0dda18d6712d66a1eea4cde7afc289ed4
toeten <n.santiago0228@gmail.com>
2024-10-18 19:04:36 -0400

    migration to remove event history and add simulationid to randomevents table

--- server/models/Simulation.js
+++ server/models/Simulation.js
@@ -1,11 +1,11 @@
 const knex = require('../db/knex');
-const authUtils = require('../utils/auth-utils');
+
 
 class Simulation {
   
-  constructor({ id, simulation_id, curr_month, year_complete, total_networth}) {
+  constructor({ id, user_id, curr_month, year_complete, total_networth}) {
     this.id = id;
-    this.simulation_id = simulation_id;
+    this.user_id = user_id;
     this.curr_month = curr_month;
     this.year_complete = year_complete;
     this.total_networth = total_networth;
