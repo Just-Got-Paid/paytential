20296c7fed21b3d5d9cca2296a19c3298e61b27a
toeten <n.santiago0228@gmail.com>
2024-10-18 19:12:19 -0400

    created methods for organization model GET and DELETE student

--- server/models/Organization.js
+++ server/models/Organization.js
@@ -3,10 +3,30 @@ const authUtils = require('../utils/auth-utils');
 
 class Organization {
   
-  constructor({ id, username, organization_id }) {
+  constructor({ id,name }) {
     this.id = id;
-    this.username = username;
-    this.organization_id = organization_id;
+    this.name = name;
+  }
+  static async removeStudentFromOrganization(organization_id, student_id) {
+    try {
+      const student = await knex('users')
+        .where({ id: student_id, organization_id: organization_id, role: 'student' })
+        .first();
+      if (!student) throw new Error('Student not found in this organization');
+      
+      await knex('users').where({ id: student_id }).del();
+      return student;
+    } catch (error) {
+      throw new Error('Error removing student from organization: ' + error.message);
+    }
+  }
+  static async getStudentsByOrganization(organization_id) {
+    try {
+      const students = await knex('users').where({ organization_id: organization_id, role: 'student' });
+      return students;
+    } catch (error) {
+      throw new Error('Error fetching students from organization: ' + error.message);
+    }
   }
 }
 
